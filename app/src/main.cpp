915d64ce1c74f5f77ee1b0db0c9365a76a4fe324
Julian <tuptusstokrotka@gmail.com>
2026-04-30 21:30:02 +0200

    build: board: add custom board our_board by rename

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -4,7 +4,7 @@
 
 /* The devicetree node identifier for the "led0" alias. */
 // #define LED_NODE DT_ALIAS(led0)
-#define LED_NODE DT_ALIAS(app_led)
+#define LED_NODE DT_ALIAS(app_led) // Add flag: -DEXTRA_DTC_OVERLAY_FILE="boards/app.overlay"
 
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
 
